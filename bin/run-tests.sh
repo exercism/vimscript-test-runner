bbcb5d17da78d2a48b2a76e9f528d1ba46c1c0e3
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-08 10:02:44 +0200

    Simplify test script

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -15,29 +15,25 @@ exit_code=0
 
 # Iterate over all test directories
 for test_dir in tests/*; do
-    test_dir_name="$(basename $test_dir)"
-    test_dir_path="$(realpath $test_dir)"
-    results_file="results.json"
-    results_file_path="${test_dir}/results.json"
-    expected_results_file="expected_results.json"
-    expected_results_file_path="${test_dir}/expected_results.json"    
+    test_dir_name=$(basename "${test_dir}")
+    test_dir_path=$(realpath "${test_dir}")
+    results_file_path="${test_dir_path}/results.json"
+    expected_results_file_path="${test_dir_path}/expected_results.json"
 
-    if [ "${test_dir_name}" != "output" ] && [ -f "${expected_results_file_path}" ]; then
-        bin/run.sh "${test_dir_name}" "${test_dir}" "${test_dir}"
+    bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
 
-        # Normalize the results file
-        sed -i -E \
-            -e 's/Elapsed time: [0-9]+\.[0-9]+ sec\.//' \
-            -e 's~/tmp/[^/]+/[^,]+,\s*~~g' \
-            -e "s~${test_dir_path}~/solution~g" \
-            "${results_file_path}"
+    # Normalize the results file
+    sed -i -E \
+        -e 's/Elapsed time: [0-9]+\.[0-9]+ sec\.//' \
+        -e 's~/tmp/[^/]+/[^,]+,\s*~~g' \
+        -e "s~${test_dir_path}~/solution~g" \
+        "${results_file_path}"
 
-        echo "${test_dir_name}: comparing ${results_file} to ${expected_results_file}"
-        diff "${results_file_path}" "${expected_results_file_path}"
+    echo "${test_dir_name}: comparing results.json to expected_results.json"
+    diff "${results_file_path}" "${expected_results_file_path}"
 
-        if [ $? -ne 0 ]; then
-            exit_code=1
-        fi
+    if [ $? -ne 0 ]; then
+        exit_code=1
     fi
 done
 
