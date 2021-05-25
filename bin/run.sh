cb43aa36bf5ec3d56e4661464d219f424f17048a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-05-25 12:35:08 +0200

    Use `message` key instead of `output` key in `results.json` files (#2)

--- bin/run.sh
+++ bin/run.sh
@@ -51,7 +51,7 @@ else
          | GREP_COLOR='01;31' grep --color=always -E -e '.*\[EXECUTE\] \(X\).*|$' \
          | GREP_COLOR='01;32' grep --color=always -E -e '.*\[EXECUTE\].*|$')
 
-    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
+    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
 fi
 
 echo "${slug}: done"
